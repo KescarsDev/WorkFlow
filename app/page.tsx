3806f8fb812e5d684c089520cc23eb4eabaaef7d
KescarsDev <kescarsdev@gmail.com>
2024-01-03 17:31:38 +0100

    new updating

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import Link from 'next/link'
 
-export default function General () {
-    return (
-        <>
-            General page...
-        </>
-        )
+export default function Home() {
+  return (
+    <>
+    <div className='flex flex-col items-center justify-center min-h-screen'>
+        <h2 className='text-3xl font-semibold mb-4'>
+            WorkFlow App
+        </h2>
+        <Link href={'/user-space/general/dashboard'}>
+            {"Vers l'application"}
+        </Link>
+    </div>
+    </>
+    )
 }
