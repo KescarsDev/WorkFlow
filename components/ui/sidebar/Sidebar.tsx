ae347843d79844c95128aa606bde99f2fffe9e09
KonJud <deveuudes@gmail.com>
2024-01-10 13:34:32 +0100

    presque fin de la factorisation de la sidebar

--- components/ui/sidebar/Sidebar.tsx
+++ components/ui/sidebar/Sidebar.tsx
@@ -1,11 +1,19 @@
+'use client'
+
 import React from 'react'
 import Link from 'next/link'
 
 import {
+    ChevronLeft,
     Workflow,
-    
 } from 'lucide-react'
+import { GrOverview } from "react-icons/gr"
+import { MdAddIcCall } from "react-icons/md"
+import { IoPeopleCircleOutline } from "react-icons/io5"
+import { FaFileInvoice } from "react-icons/fa6"
+import { FaMailchimp } from "react-icons/fa6"
 import { SidebarItem } from '@/types/AppLinks'
+import SidebarDropdownLinks from './SidebarDopdownLinks'
 
 export default function Sidebar () {
 
@@ -37,14 +45,44 @@ export default function Sidebar () {
                 {/* Links */}
                 <nav className='flex flex-col gap-8 px-3 py-6'>
                     <Link
-                        href={'/user-space/general/dashbord'}
-                        className='flex space-x-2 hover:bg-primary-hoverLink'
+                        href={'/user-space/general/dashboard'}
+                        className='flex space-x-2 hover:bg-primary-hoverLink p-3 rounded-md animate'
                     >
-                        <
+                        <GrOverview className='w-6 h-6' />
                         <span>Général</span>
                     </Link>
+                    <Link
+                        href={'/user-space/call'}
+                        className='flex space-x-2 hover:bg-primary-hoverLink p-3 rounded-md animate'
+                    >
+                        <MdAddIcCall className='w-6 h-6' />
+                        <span>Appel</span>
+                    </Link>
+                    <Link
+                        href={'/user-space/visit'}
+                        className='flex space-x-2 hover:bg-primary-hoverLink p-3 rounded-md animate'
+                    >
+                        <IoPeopleCircleOutline className='w-6 h-6' />
+                        <span>Visite</span>
+                    </Link>
+                    <Link
+                        href={'/user-space/visit'}
+                        className='flex space-x-2 hover:bg-primary-hoverLink p-3 rounded-md animate'
+                    >
+                        <FaFileInvoice className='w-6 h-6' />
+                        <span>Courrier</span>
+                    </Link>
+                    <SidebarDropdownLinks 
+                        label='Facturation'
+                        items={invoicingLinks}
+                        icon={{icon: FaMailchimp}}
+                    />
                 </nav>
             </div>
+            {/* Sidiebar Footer */}
+            <button className='flex items-center justify-center gap-2 py-3 bg-my-primary-500'>
+                <ChevronLeft />
+            </button>
         </div>
     )
 }
--- components/ui/sidebar/SidebarDopdownLinks.tsx
+++ components/ui/sidebar/SidebarDopdownLinks.tsx
@@ -8,13 +8,14 @@ import {
     CollapsibleTrigger
 } from '@/components/ui/collapsible'
 
-import { ChevronRight } from 'lucide-react'
+import { ChevronsUpDown } from 'lucide-react'
 import { IconProps } from '@/types/IconsProps'
 import CollapsibleLink from './CollapsibleLink'
+import { SidebarItem } from '@/types/AppLinks'
 
 interface SidebarDropdownLinksProps {
     label: string
-    items: string[]
+    items: SidebarItem[]
     icon: IconProps
 }
 export default function SidebarDropdownLinks ({
@@ -25,12 +26,12 @@ export default function SidebarDropdownLinks ({
 
     return(
         <Collapsible>
-            <CollapsibleTrigger className='flex justify-between items-center w-full'>
-                <div className='flex space-x-2 p-1'>
+            <CollapsibleTrigger className='flex justify-between items-center w-full hover:bg-primary-hoverLink p-3 rounded-md animate'>
+                <div className='flex space-x-2'>
                     <icon.icon className='w-5 h-5' />
                     <span>{label}</span>
                 </div>
-                <ChevronRight className='w-4 h-4' />
+                <ChevronsUpDown className='w-4 h-4 font-semibold' />
             </CollapsibleTrigger>
             <CollapsibleContent>
                 {
